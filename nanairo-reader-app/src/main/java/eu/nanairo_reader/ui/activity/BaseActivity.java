fb5e96a8654c2f5df7fdbae65bd9576d7cb1c17d
eisakuu <eisakuu@mac>
2013-07-12 23:56:26 +0900

    抽象クラスに変更

--- nanairo-reader-app/src/main/java/eu/nanairo_reader/ui/activity/BaseActivity.java
+++ nanairo-reader-app/src/main/java/eu/nanairo_reader/ui/activity/BaseActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import eu.nanairo_reader.NanairoApplication;
 
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
