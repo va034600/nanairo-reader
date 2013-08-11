8049be77695edfa2fef9d3a207786873d62c0824
eisakuu <eisakuu@mac>
2013-08-11 21:35:16 +0900

    fixed #36

--- nanairo-reader-app/src/main/java/com/gmail/va034600/nreader/ui/activity/BaseActivity.java
+++ nanairo-reader-app/src/main/java/com/gmail/va034600/nreader/ui/activity/BaseActivity.java
@@ -2,13 +2,13 @@ package com.gmail.va034600.nreader.ui.activity;
 
 import javax.inject.Inject;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
 import com.gmail.va034600.nreader.business.domain.service.RssService;
 import com.gmail.va034600.nreader.ui.NanairoApplication;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends ActionBarActivity {
 	@Inject
 	RssService rssService;
 
